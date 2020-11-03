f9ce1290d10a9036bd7cd01f0b6f8ff74fa7d871
unknown <mittalshubham05@gmail.com>
2020-11-03 22:23:31 +0530

    Edit: provided functionality to calculate monthly wages using loop statements

--- employeeWage.js
+++ employeeWage.js
@@ -4,11 +4,24 @@ const FULL_TIME =2;
 const PER_HOUR_WAGE = 20;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
+const MAX_HOURS_MONTH = 120;
+const MAX_DAYS_MONTH = 20;
+let workingDays = 0;
 let employeeHours = 0;
-let employeeType = Math.floor(Math.random()*10)%3;
-employeeHours = getWorkHours(employeeType);
-let empWage = employeeHours*PER_HOUR_WAGE;
-console.log("Employee Wage : " + empWage);
+let totalEmployeeHours = 0;
+let monthlyWage = 0;
+while(workingDays < MAX_DAYS_MONTH && totalEmployeeHours < MAX_HOURS_MONTH){
+    let employeeType = Math.floor(Math.random()*10)%3;
+    if(employeeType == 0)
+        continue;
+    employeeHours = getWorkHours(employeeType);
+    totalEmployeeHours += employeeHours;
+    let empWage = employeeHours*PER_HOUR_WAGE;
+    monthlyWage += empWage;
+    workingDays++;
+}
+console.log("Days: " + workingDays + " and Hours: " + totalEmployeeHours + " Employee Wage for the month: " + monthlyWage);
+
 
 
 function getWorkHours(employeeType){
