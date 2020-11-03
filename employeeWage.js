beac7a5be781c02398f93575b623bccdd7de44c8
unknown <mittalshubham05@gmail.com>
2020-11-03 22:02:00 +0530

    Edit: check for part time or full time employee and calculate wage

--- employeeWage.js
+++ employeeWage.js
@@ -1,7 +1,18 @@
-const is_Absent = 1;
-absentOrPresent = Math.floor(Math.random()*10)%2;
-console.log(absentOrPresent);
-if(absentOrPresent == is_Absent) 
-    console.log("Employee is absent");
-else
-    console.log("Employee is present");
+const NO_TIME = 0;
+const PART_TIME =1;
+const FULL_TIME =2;
+const PER_HOUR_WAGE = 20;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+let employeeHours = 0;
+employeeType = Math.floor(Math.random()*10)%3;
+switch(employeeType){
+    case 1: employeeHours = PART_TIME_HOURS;
+            break;
+    case 2: employeeHours = FULL_TIME_HOURS;
+            break;
+    default: employeeHours = 0;
+            break;
+}
+let empWage = employeeHours*PER_HOUR_WAGE;
+console.log("Employee Wage : " + empWage);
