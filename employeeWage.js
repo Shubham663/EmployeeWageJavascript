f71214c9325e458b83e17beab0a61ee171114ce4
unknown <mittalshubham05@gmail.com>
2020-11-03 22:08:38 +0530

    Edit: provided functionality for getting empHours using function

--- employeeWage.js
+++ employeeWage.js
@@ -5,14 +5,21 @@ const PER_HOUR_WAGE = 20;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 let employeeHours = 0;
-employeeType = Math.floor(Math.random()*10)%3;
-switch(employeeType){
-    case 1: employeeHours = PART_TIME_HOURS;
-            break;
-    case 2: employeeHours = FULL_TIME_HOURS;
-            break;
-    default: employeeHours = 0;
-            break;
-}
+let employeeType = Math.floor(Math.random()*10)%3;
+employeeHours = getWorkHours(employeeType);
 let empWage = employeeHours*PER_HOUR_WAGE;
 console.log("Employee Wage : " + empWage);
+
+
+function getWorkHours(employeeType){
+    let employeeHours = 0;
+    switch(employeeType){
+        case 1: employeeHours = PART_TIME_HOURS;
+            break;
+        case 2: employeeHours = FULL_TIME_HOURS;
+            break;  
+        default: employeeHours = 0;
+            break;
+    }
+    return employeeHours;
+}
