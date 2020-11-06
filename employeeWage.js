a410584ac0fa747ecb0b2211151542cf0606ab35
unknown <mittalshubham05@gmail.com>
2020-11-06 16:57:43 +0530

    Add: added functionality for calculating monthly wage using map

--- employeeWage.js
+++ employeeWage.js
@@ -9,9 +9,13 @@ const MAX_DAYS_MONTH = 20;
 let workingDays = 0;
 let employeeHours = 0;
 let monthlyWage = 0;
+let monthlyWageMap = 0;
 let totalEmployeeHours = 0;
 let employeeWageArray = new Array();
+let dayToDailyWageMap = new Map();
+let counterMap = 0;
 while(workingDays < MAX_DAYS_MONTH && totalEmployeeHours < MAX_HOURS_MONTH){
+    counterMap++;
     let employeeType = Math.floor(Math.random()*10)%3;
     if(employeeType == 0)
         continue;
@@ -19,11 +23,16 @@ while(workingDays < MAX_DAYS_MONTH && totalEmployeeHours < MAX_HOURS_MONTH){
     totalEmployeeHours += employeeHours;
     let empWage = employeeHours*PER_HOUR_WAGE;
     employeeWageArray.push(empWage);
+    dayToDailyWageMap.set(counterMap,empWage);
     workingDays++;
 }
 
 function sum(dailyWage){
     monthlyWage += dailyWage;
+}
+
+function sumMap(value,key){
+    monthlyWageMap += value;
 }
 
 function monthlyWageReduce(total,dailyWageReduce){
@@ -54,6 +63,8 @@ function partTimeOccurence(dailyWage){
 
 employeeWageArray.forEach(sum);
 let totalWageReduce = employeeWageArray.reduce(monthlyWageReduce,0);
+dayToDailyWageMap.forEach(sumMap);
+console.log("Days: " + workingDays + " and Hours: " + totalEmployeeHours + " Employee Wage for the month using map: " + monthlyWageMap);
 console.log("Days: " + workingDays + " and Hours: " + totalEmployeeHours + " Employee Wage for the month using forEach: " + monthlyWage);
 console.log("Days: " + workingDays + " and Hours: " + totalEmployeeHours + " Employee Wage for the month using reduce: " + totalWageReduce);
 let dayWageMap = employeeWageArray.map(daysToWages);
